4b1155f6af2381455bf3e66ae4722127735983f4
Krys Lawrence <krys@krys.ca>
2025-04-23 21:18:25 -0400

    test: :white_check_mark: add placeholder code and test
    - This is to make pytest and coverage happy for now.
    - Replace this stuff once real code is added.

---
+++ src/aquarion_tts/placeholder.py
@@ -0,0 +1,6 @@
+"""Placeholder test module so that there is something to test."""
+
+
+def placeholder() -> str:
+    """Return a placeholder string for testing."""
+    return "I am a placeholder"
---
+++ tests/placeholder_test.py
@@ -0,0 +1,7 @@
+"""Placeholder test module."""
+
+from aquarion_tts import placeholder
+
+
+def test_placeholder() -> None:
+    assert placeholder.placeholder() == "I am a placeholder"
