5b796b7e514c43c97b63c72bc67b78db1ee49566
Krys Lawrence <krys@krys.ca>
2025-05-09 22:39:35 -0400

    feat(api): :sparkles: add TTSBackend interface protocol
    TTS backend are required to convert test to speech data.

---
+++ tests/api/ttsbackend_test.py
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: 2025-present Krys Lawrence <aquarion.5.krystopher@spamgourmet.org>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# Part of the aquarion-libtts library of the Aquarion AI project.
+# Copyright (C) 2025-present Krys Lawrence <aquarion.5.krystopher@spamgourmet.org>
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+"""Unit tests for the TTSBackend protocol.
+
+These tests serve mostly to document the expectations of all TTSBackend implementations.
+"""
+
+from typing import assert_type
+
+import pytest
+
+from aquarion.libs.libtts.api.ttsbackend import ITTSBackend
+from aquarion.libs.libtts.api.ttsspeechdata import TTSSpeechData
+
+
+class DummyTTSBackend:
+    """Dummy TTS Backend to test the protocol.
+
+    Specific implementations here do not matter, the only important thing is to conform
+    to the TTSSettings protocol.
+    """
+
+    def convert(self, text: str) -> TTSSpeechData:
+        return TTSSpeechData(
+            audio=f"some audio of {text}".encode(), mime_type="audio/wav"
+        )
+
+
+def assert_typecheck_conforms_to_ittsbackend_protocol(
+    backend: ITTSBackend,
+) -> ITTSBackend:
+    """Enable typechecker validation of conformance to the ITTSBackend protocol.
+
+    Also does a bit of runtime checking.
+    """
+    assert_type(backend, ITTSBackend)  # Assert at typecheck time
+    assert isinstance(backend, ITTSBackend)  # Assert at runtime time
+    return backend
+
+
+def test_ittsbackend_should_conform_to_ittsbackend_protocol() -> None:
+    backend = DummyTTSBackend()
+    assert_typecheck_conforms_to_ittsbackend_protocol(backend)
+
+
+def test_ttsbackend_convert_should_require_some_text_input() -> None:
+    backend = DummyTTSBackend()
+    with pytest.raises(TypeError, match="missing .* required positional argument"):
+        backend.convert()  # type: ignore [call-arg]
+
+
+def test_ttsbackend_convert_should_return_a_ttsspeechdata_object() -> None:
+    backend = DummyTTSBackend()
+    speech_data = backend.convert("some text")
+    assert isinstance(speech_data, TTSSpeechData)
